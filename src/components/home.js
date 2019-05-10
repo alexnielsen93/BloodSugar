a5e313d31bfa3aadebe5b0e24f92834bf261d655
Alex Nielsen <alexnielsen@Alexs-MacBook-Pro-2.local>
2019-05-10 15:56:53 -0600

    sass imported

--- src/components/home.js
+++ src/components/home.js
@@ -5,10 +5,16 @@ import {Link} from 'react-router-dom'
 
 const Home=  ()=>(
 
-  <div>
-    <Link to ='/add_data'>Enter Blood Sugar</Link>
+  <div >
+
+    <div className = "link-boxes"><Link to ='/add_data'>Enter Blood Sugar</Link>
     <Link to ='/data'>Data</Link>
-    <Link to = '/graph'>Graph</Link>
+    <Link to = '/graph'>Graph</Link></div>
+  
+  <div className = 'hero-box'>
+  
+  
+  </div>
   </div>
 )
 
